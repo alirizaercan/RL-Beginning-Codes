0a011086b56dc93c3a082e0326b78df030acb62a
Ali Kösemen <alikosemen1@gmail.com>
2026-03-25 12:12:21 +0300

    feat(lunar_lander_llm): simplify test script to use LLM-based action selection

--- lunar_lander_llm/test.py
+++ lunar_lander_llm/test.py
@@ -1,17 +1,13 @@
-from stable_baselines3 import PPO
 from env import LunarLanderEnv
 
 env = LunarLanderEnv(render_mode="human")
-
-model = PPO.load("ppo_lunarlander_llm", device="cpu")
 
 observation, info = env.reset()
 total_reward = 0
 episode = 1
 
 while True:
-    action, _states = model.predict(observation, deterministic=True)
-    observation, reward, terminated, truncated, info = env.step(action)
+    observation, reward, terminated, truncated, info = env.step()
     total_reward += reward
 
     if terminated or truncated:
